e6c8677d98cba6f8fbd9a05f1ee75956525b6758
Ryan Wang <i@ryanc.cc>
2022-11-09 22:49:14 +0800

    feat: add setup color scheme support

--- src/main.ts
+++ src/main.ts
@@ -42,3 +42,40 @@ export function openSearch() {
   }
   window.SearchWidget.open();
 }
+
+type ColorSchemeType = "system" | "dark" | "light";
+
+export let currentColorScheme: ColorSchemeType = "system";
+
+export function initColorScheme(defaultColorScheme: ColorSchemeType, enableChangeColorScheme: boolean) {
+  let colorScheme = defaultColorScheme;
+
+  if (enableChangeColorScheme) {
+    colorScheme = (localStorage.getItem("color-scheme") as ColorSchemeType) || defaultColorScheme;
+  }
+
+  currentColorScheme = colorScheme;
+
+  setColorScheme(colorScheme, false);
+}
+
+export function setColorScheme(colorScheme: ColorSchemeType, store: boolean) {
+  if (colorScheme === "system") {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    document.documentElement.classList.add(prefersDark ? "dark" : "light");
+    document.documentElement.classList.remove(prefersDark ? "light" : "dark");
+  } else {
+    document.documentElement.classList.add(colorScheme);
+    document.documentElement.classList.remove(colorScheme === "dark" ? "light" : "dark");
+  }
+  currentColorScheme = colorScheme;
+  if (store) {
+    localStorage.setItem("color-scheme", colorScheme);
+  }
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function () {
+  if (currentColorScheme === "system") {
+    setColorScheme("system", false);
+  }
+});
