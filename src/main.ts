64af9838d872aa5c082cabf59063477d266a9ae6
Ryan Wang <i@ryanc.cc>
2024-08-22 12:23:54 +0800

    feat: add scroll to top button
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/main.ts
+++ src/main.ts
@@ -89,3 +89,31 @@ window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", fun
     setColorScheme("system", false);
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollToTopButton = document.getElementById("btn-scroll-to-top");
+
+  if (!scrollToTopButton) {
+    return;
+  }
+
+  scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  window.addEventListener("scroll", _handleScroll);
+
+  function _handleScroll() {
+    if (!scrollToTopButton) {
+      return;
+    }
+
+    const isDown = window.scrollY > 300;
+
+    if (isDown) {
+      scrollToTopButton.style.opacity = "1";
+    } else {
+      scrollToTopButton.style.opacity = "0";
+    }
+  }
+});
